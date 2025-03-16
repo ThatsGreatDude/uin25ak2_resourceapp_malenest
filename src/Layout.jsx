6a33792923ee4e244d189d92b91a4fd66f7c0524
ThatsGreatDude <malenest@hiof.no>
2025-03-16 01:51:32 +0100

    layout
    Tok inn html fila fra arbeidskrav 1 inn i layout

---
+++ src/Layout.jsx
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arbeidskrav 1 Ressursarkiv</title>
+    <link href="styles.css" rel="stylesheet" media="all">
+    <script src="ressurser.js" defer></script>
+    <script src="script.js" defer></script>
+</head>
+
+<body>
+    <main>
+        <nav>
+            <ul>
+                <li><a href="#" data-category="HTML">HTML</a></li> <!--https://www.w3schools.com/tags/att_global_data.asp-->
+                <li><a href="#" data-category="CSS">CSS</a></li>
+                <li><a href="#" data-category="JavaScript">JavaScript</a></li>
+                <li><a href="#" data-category="React">React</a></li>
+                <li><a href="#" data-category="Sanity and headless CMS">Sanity and headless CMS</a></li>
+            </ul>
+        </nav>
+
+        <section id="category-info-container">
+            <article id="category-info"></article>
+        </section>
+    </main>
+</body>
+</html>
