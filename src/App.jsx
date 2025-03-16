b63e872662eb166f79438ff40734150996beeb80
ThatsGreatDude <malenest@hiof.no>
2025-03-16 17:03:04 +0100

    main
    endret til at import router.. er i main.jsx som på eksempelet på github

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./komponenter/Layout";
- import Resources from "./komponenter/Resources";
-import "./App.css";
+import Resources from "./komponenter/Resources";
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<h1>Ressursarkiv Test</h1>} />
-          <Route path="/html" element={<Resources category="html" />} />
-          <Route path="/css" element={<Resources category="css" />} />
-          <Route path="/javascript" element={<Resources category="javascript" />} />
-          <Route path="/react" element={<Resources category="react" />} />
-          <Route path="/sanity" element={<Resources category="headless-cms" />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<h1>ressursarkiv.......</h1>} />
+        <Route path="/html" element={<Resources category="html" />} />
+        <Route path="/css" element={<Resources category="css" />} />
+        <Route path="/javascript" element={<Resources category="javascript" />} />
+        <Route path="/react" element={<Resources category="react" />} />
+        <Route path="/sanity" element={<Resources category="headless-cms" />} />
+      </Routes>
+    </Layout>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Endret slik at router kun er her, og ikke i app.jsx som på github https://github.com/toremake/uin2025_coursebase/blob/main/Zelda_api/src/main.jsx
+import "./index.css"; 
+import App from "./App.jsx"; 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
